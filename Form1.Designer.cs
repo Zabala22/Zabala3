5e122f4a6767979593a922d657417e4bb9501fec
Zabala22 <zabalaemmanuel32@gmail.com>
2022-07-16 22:01:38 +0800

    layout

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace contact_tracing_program
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CTtitle = new System.Windows.Forms.Label();
+            this.CTlabelname = new System.Windows.Forms.Label();
+            this.CTlabeldate = new System.Windows.Forms.Label();
+            this.CTlabelnumber = new System.Windows.Forms.Label();
+            this.CTlabelemail = new System.Windows.Forms.Label();
+            this.CTlabelsex = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CTtitle
+            // 
+            this.CTtitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CTtitle.AutoSize = true;
+            this.CTtitle.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CTtitle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.CTtitle.Location = new System.Drawing.Point(51, 22);
+            this.CTtitle.Name = "CTtitle";
+            this.CTtitle.Size = new System.Drawing.Size(418, 27);
+            this.CTtitle.TabIndex = 0;
+            this.CTtitle.Text = "COVID-19 Contact Tracing Form";
+            this.CTtitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CTtitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CTlabelname
+            // 
+            this.CTlabelname.AutoSize = true;
+            this.CTlabelname.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CTlabelname.Location = new System.Drawing.Point(39, 81);
+            this.CTlabelname.Name = "CTlabelname";
+            this.CTlabelname.Size = new System.Drawing.Size(44, 17);
+            this.CTlabelname.TabIndex = 1;
+            this.CTlabelname.Text = "Name";
+            this.CTlabelname.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // CTlabeldate
+            // 
+            this.CTlabeldate.AutoSize = true;
+            this.CTlabeldate.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CTlabeldate.Location = new System.Drawing.Point(39, 156);
+            this.CTlabeldate.Name = "CTlabeldate";
+            this.CTlabeldate.Size = new System.Drawing.Size(98, 17);
+            this.CTlabeldate.TabIndex = 2;
+            this.CTlabeldate.Text = "Birth Date";
+            this.CTlabeldate.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // CTlabelnumber
+            // 
+            this.CTlabelnumber.AutoSize = true;
+            this.CTlabelnumber.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CTlabelnumber.Location = new System.Drawing.Point(274, 81);
+            this.CTlabelnumber.Name = "CTlabelnumber";
+            this.CTlabelnumber.Size = new System.Drawing.Size(134, 17);
+            this.CTlabelnumber.TabIndex = 3;
+            this.CTlabelnumber.Text = "Contact Number";
+            // 
+            // CTlabelemail
+            // 
+            this.CTlabelemail.AutoSize = true;
+            this.CTlabelemail.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CTlabelemail.Location = new System.Drawing.Point(274, 156);
+            this.CTlabelemail.Name = "CTlabelemail";
+            this.CTlabelemail.Size = new System.Drawing.Size(53, 17);
+            this.CTlabelemail.TabIndex = 4;
+            this.CTlabelemail.Text = "Email";
+            this.CTlabelemail.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // CTlabelsex
+            // 
+            this.CTlabelsex.AutoSize = true;
+            this.CTlabelsex.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CTlabelsex.Location = new System.Drawing.Point(39, 235);
+            this.CTlabelsex.Name = "CTlabelsex";
+            this.CTlabelsex.Size = new System.Drawing.Size(35, 17);
+            this.CTlabelsex.TabIndex = 5;
+            this.CTlabelsex.Text = "Sex";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 23);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(274, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(215, 23);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(274, 176);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(215, 23);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(39, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Last Name, First Name M.I.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(274, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "example@example.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(274, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "0000-000-0000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(39, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "MM/DD/YYYY";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(39, 255);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(52, 19);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Male";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(116, 255);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(64, 19);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "Female";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(151, 312);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(215, 203);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 521);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Generate QR Code";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 809);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CTlabelsex);
+            this.Controls.Add(this.CTlabelemail);
+            this.Controls.Add(this.CTlabelnumber);
+            this.Controls.Add(this.CTlabeldate);
+            this.Controls.Add(this.CTlabelname);
+            this.Controls.Add(this.CTtitle);
+            this.Name = "Form1";
+            this.Text = "COVID CTF";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label CTtitle;
+        private Label CTlabelname;
+        private Label CTlabeldate;
+        private Label CTlabelnumber;
+        private Label CTlabelemail;
+        private Label CTlabelsex;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private PictureBox pictureBox1;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace contact_tracing_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
